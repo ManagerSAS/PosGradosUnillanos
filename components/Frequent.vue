77e754653d9d2b3ab6dc2575d3d45d1ca42ea753
[200~ <desarrollo@agenciamanager.com>
2022-12-14 17:46:40 -0500

    Actualizacion 14/12/2022 17:46

--- components/Frequent.vue
+++ components/Frequent.vue
@@ -22,9 +22,9 @@
                         accordion 
                         height="200"
                         class="text-blue-f-1em">
-                        <v-expansion-panel-header style="color:#003B4C;" >2. ¿Cuánto duran los posgrados?</v-expansion-panel-header>
+                        <v-expansion-panel-header style="color:#003B4C;" >2. ¿Cuál es la duración de los posgrados?</v-expansion-panel-header>
                         <v-expansion-panel-content>
-                            Como es común las especializaciones tienen una durabilidad de 2 semestres y maestrías de 4 semestres, el doctorado está sujeto a la duración de la investigación realizada. 
+                            Las especializaciones tienen una durabilidad de 2 semestres y maestrías de 4 semestres, el doctorado está sujeto a la duración de la investigación realizada
                         </v-expansion-panel-content>
                     </v-expansion-panel>
                     <v-expansion-panel 
@@ -40,18 +40,18 @@
                         accordion 
                         height="200"
                         class="text-blue-f-1em">
-                        <v-expansion-panel-header style="color:#003B4C;" >4. ¿Cuál es el valor de la inscripción?</v-expansion-panel-header>
+                        <v-expansion-panel-header style="color:#003B4C;" >4. ¿Cuál es el valor inscripción?</v-expansion-panel-header>
                         <v-expansion-panel-content>
-                            El valor de inscripción es de $166.700 
+                            El valor de inscripción es de $166.700 COP 
                         </v-expansion-panel-content>
                     </v-expansion-panel>
                     <v-expansion-panel 
                         accordion 
                         height="200"
                         class="text-blue-f-1em">
-                        <v-expansion-panel-header style="color:#003B4C;" >5. ¿Dónde puedo realizar el pago de inscripción y matricula?</v-expansion-panel-header>
+                        <v-expansion-panel-header style="color:#003B4C;" >5. ¿Dónde puedo realizar el pago de inscripción y matrícula?</v-expansion-panel-header>
                         <v-expansion-panel-content>
-                            Sencillo, si pagas en efectivo puedes acudir al Banco Bogotá, Banco caja social, Bancolombia, Congente, BBVA. En pagos virtuales solo realizar la transacción con tu tarjeta crédito o realizar el pago por PSE
+                            Sencillo, Si realizas el pago en efectivo puedes acudir al Banco Bogotá, Banco caja social, Bancolombia, Congente, BBVA. En pagos virtuales solo realizar la transacción con tu tarjeta crédito o realizar el pago por PSE
                         </v-expansion-panel-content>
                     </v-expansion-panel>
                     <v-expansion-panel 
@@ -78,7 +78,7 @@
                         class="text-blue-f-1em">
                         <v-expansion-panel-header style="color:#003B4C;" >8. ¿Cómo puedo financiar mi posgrado?</v-expansion-panel-header>
                         <v-expansion-panel-content>
-                            La universidad de los Llanos tiene convenio con el Banco Pichincha y con el ICETEX en la página Web www.Icetex.gov.co – crédito tú eliges – Posgrados. ¡Finánciate tu posgrado!
+                            La Universidad de los Llanos tiene convenio con el Banco Pichincha y con el ICETEX en la página Web www.Icetex.gov.co – crédito tú eliges – Posgrados. ¡Finánciate tu posgrado!
                         </v-expansion-panel-content>
                     </v-expansion-panel>
                     <v-expansion-panel 
@@ -87,14 +87,14 @@
                         class="text-blue-f-1em">
                         <v-expansion-panel-header style="color:#003B4C;" >9. ¿Cuáles son las fechas de inscripción?</v-expansion-panel-header>
                         <v-expansion-panel-content>
-                            <a href="https://siau.unillanos.edu.co:8443/ORION/Preregistro " target="_blank" rel="noopener noreferrer">¡Inscríbete ahora!</a>  Tienes hasta el 27 de enero de 2023 el cierre del formulario de inscripción en plataforma es el 31 de enero 2023
+                            Tienes hasta el 27 de enero de 2023 el cierre del formulario de inscripción en plataforma es el 31 de enero 2023 <a href="https://siau.unillanos.edu.co:8443/ORION/Preregistro " target="_blank" rel="noopener noreferrer">¡Inscríbete ahora!</a>  
                         </v-expansion-panel-content>
                     </v-expansion-panel>
                     <v-expansion-panel 
                         accordion 
                         height="200"
                         class="text-blue-f-1em">
-                        <v-expansion-panel-header style="color:#003B4C;" >10. Soy estudiante de pregrado y quiero realizar un posgrado como opción de grado, ¿Qué debo hacer? </v-expansion-panel-header>
+                        <v-expansion-panel-header style="color:#003B4C;" >10. Soy estudiante de pregrado y quiero realizar un posgrado como opción de grado, ¿qué debo hacer? </v-expansion-panel-header>
                         <v-expansion-panel-content>
                             Realiza el proceso con tu director de programa correspondiente para acceder a la opción de grado y la universidad te otorga un descuento del 20% sobre el valor de inscripción por educación continuada con la Unillanos.
                         </v-expansion-panel-content>
